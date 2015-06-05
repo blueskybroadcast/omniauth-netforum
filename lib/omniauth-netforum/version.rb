ff464e262f1043a0a3f8f87d48a6cf6bd5d93c39
Timm Liu <tliu@blueskybroadcast.com>
2015-06-05 16:32:27 -0700

    Fix signing out and signing in resulting in 500 error.

--- lib/omniauth-netforum/version.rb
+++ lib/omniauth-netforum/version.rb
@@ -1,5 +1,5 @@
 module Omniauth
   module Netforum
-    VERSION = "0.1.2"
+    VERSION = "0.1.3"
   end
 end
--- lib/omniauth/strategies/netforum.rb
+++ lib/omniauth/strategies/netforum.rb
@@ -1,7 +1,6 @@
 module OmniAuth
   module Strategies
     class Netforum < OmniAuth::Strategies::OAuth2
-
       option :name, 'netforum'
 
       option :client_options, {
@@ -10,6 +9,7 @@ module OmniAuth
         authorize_url: '/eWeb/ValidateLogin.aspx',
         authentication_wsdl: '/xWeb/Signon.asmx?WSDL',
         user_info_wsdl: '/xweb/netFORUMXMLONDemand.asmx?WSDL',
+        event_code: 'MUST BE SET',
         username: 'MUST BE SET',
         password: 'MUST BE SET'
       }
@@ -20,9 +20,13 @@ module OmniAuth
         raw_info
       end
 
+      extra do
+        { :raw_info => raw_info }
+      end
+
       def request_phase
-        site = session['omniauth.params']['eventcode']
-        redirect authorize_url + "?ReturnURL=" + callback_url + "&site=#{site}"
+        site = session['omniauth.params']['eventcode'] || client_event_code
+        redirect authorize_url + "?ReturnURL=" + URI.encode(callback_url+"?Site=#{site}") + "&Site=#{site}"
       end
 
       def callback_phase
@@ -48,6 +52,7 @@ module OmniAuth
         hash = AuthHash.new(:provider => name, :uid => uid)
         hash.info = info
         hash.credentials = creds
+        hash.extra = extra
         hash
       end
 
@@ -68,9 +73,9 @@ module OmniAuth
           on_demand_wsdl user_info_wsdl
         end
 
-        if auth = ::Netforum.authenticate(options.client_options.username, options.client_options.password)
+        if auth = ::Netforum.authenticate(client_username, client_password)
           customer_key = auth.get_customer_key(access_token)
-          auth = ::Netforum.authenticate(options.client_options.username, options.client_options.password)
+          auth = ::Netforum.authenticate(client_username, client_password)
           on_demand = ::Netforum.on_demand(auth.authentication_token)
           customer = on_demand.get_customer_by_key(customer_key)
 
@@ -91,6 +96,18 @@ module OmniAuth
 
       def authorize_url
         "#{options.client_options.site}#{options.client_options.authorize_url}"
+      end
+
+      def client_event_code
+        options.client_options.event_code
+      end
+
+      def client_username
+        options.client_options.username
+      end
+
+      def client_password
+        options.client_options.password
       end
     end
   end
